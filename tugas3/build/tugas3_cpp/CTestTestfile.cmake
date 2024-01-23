9877b11a7152c9ff67ff734827adde22df5bcd3a
nitroz23 <therealnitroz23@gmail.com>
2024-01-23 16:35:25 +0700

    build: tugas 3

---
+++ tugas3/build/tugas3_cpp/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/llnitroz23ll/magangBayuROS/src/MagangBayu24-ROS2/tugas3/src/tugas3_cpp
+# Build directory: /home/llnitroz23ll/magangBayuROS/src/MagangBayu24-ROS2/tugas3/build/tugas3_cpp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ tugas3/src/tugas3_cpp/src/pub.cpp
@@ -0,0 +1,41 @@
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+class MinimalPublisher : public rclcpp::Node
+{
+public:
+  MinimalPublisher()
+    : Node("minimal_publisher"), i(0)
+  {
+    publisher_ = create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
+    timer_ = create_wall_timer(std::chrono::seconds(2), std::bind(&MinimalPublisher::timer_callback, this));
+  }
+
+private:
+  void timer_callback()
+  {
+    auto msg = std::make_unique<geometry_msgs::msg::Twist>();
+    msg->linear.x = translasi[i];
+    // msg->linear.y = 0.0;
+    msg->angular.z = rotasi___[i];
+    publisher_->publish(std::move(msg));
+    i = (i + 1) % (sizeof(translasi) / sizeof(translasi[0]));
+  }
+
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  size_t i;
+
+  // Declarations of translasi and rotasi___ arrays
+  double translasi[13] = {2.00, 0.00, 6.28, 0.00, 4.00, 0.00, 6.28, 0.00, 3.90, 0.00, 6.28, 0.00, 2.00};
+  double rotasi___[13] = {0.00, 0.52, 3.14, 0.52, 0.00, 0.52, 3.14, 0.52, 0.00, 0.52, 3.14, 0.52, 0.00};
+};
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  auto minimal_publisher = std::make_shared<MinimalPublisher>();
+  rclcpp::spin(minimal_publisher);
+  rclcpp::shutdown();
+  return 0;
+}
